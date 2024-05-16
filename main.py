400a0829b497ee49949dd0243b7ba68cbe245278
Darius Murawski <darius@murawski.blog>
2024-05-16 21:27:27 +0200

    this looks nice now !

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-
+import plotly.graph_objects as go
 
 def get_participation(xls):
     participation = {}
@@ -65,12 +65,10 @@ def main():
 
     # Prepare the data
     df = prepare_data(participation)
-
-    # save min index for plotting
-    min_index = min(df.index)
+    df.sort_index(inplace=True)
 
     # Split the data into training and test sets
-    train = df
+    train = df[:int(0.8 * len(df))]
     test = df[int(0.8 * len(df)):]
 
     # Fit an ARIMA model to the training data
@@ -81,41 +79,96 @@ def main():
     predictions = model_fit.predict(start=min(test.index), end=max(test.index), dynamic=False, freq='W')
 
     # plot the Training Data
-    df = df.to_frame()
-    df['data_type'] = 'Training Data'
-    df.rename(columns={0: 'value'}, inplace=True)
+    train_df = train.to_frame()
+    train_df['data_type'] = 'Train Data'
+    train_df.rename(columns={0: 'value'}, inplace=True)
+    train_df.index = pd.date_range(start=min(train_df.index).end_time,  end=max(train_df.index).end_time, periods=len(train_df))
+    train_df.sort_index(inplace=True)
 
     # plot the Predictions on Test Data
     test_df = predictions.to_frame()
     test_df['data_type'] = 'Test Data'
     test_df.rename(columns={'predicted_mean': 'value'}, inplace=True)
+    test_df.index = pd.date_range(start=max(train_df.index), periods=len(test_df), freq='W')
+    test_df.sort_index(inplace=True)
 
     # plot the forecast
-    forecast = model_fit.get_forecast(steps=8)
+    forecast = model_fit.get_forecast(steps=8 + len(test))
     predicted_values = forecast.predicted_mean
     forecast_df = pd.DataFrame(predicted_values)
     forecast_df['data_type'] = 'Forecast'
     forecast_df.rename(columns={'predicted_mean': 'value'}, inplace=True)
+    forecast_df.index = pd.date_range(start=max(train.index).end_time, periods=len(forecast_df), freq='W')
+    forecast_df.sort_index(inplace=True)
     confidence_intervals = forecast.conf_int()
 
 
-    # Reset the index of the predictions DataFrame
-    # test_df = test_df.reset_index(drop=True)
-    # Concatenate the DataFrames
-    # Convert the PeriodIndex of df to a RangeIndex
-    # df.reset_index(drop=True, inplace=True)
-
-    # Concatenate the DataFrames
-    df_total = pd.concat([df, test_df, forecast_df], ignore_index=False)
-
+    # Create a new DataFrame for the confidence intervals
+    confidence_df = pd.DataFrame(confidence_intervals)
+    confidence_df['data_type'] = 'Confidence Interval'
+    confidence_df.rename(columns={0: 'lower', 1: 'upper'}, inplace=True)
     # Convert the PeriodIndex to a DateTimeIndex
-    df_total.index = pd.date_range(start=min_index.end_time, periods=len(df_total))
+    confidence_df.index = pd.date_range(start=max(train.index).end_time, periods=len(confidence_df), freq='W')
+    confidence_df.sort_index(inplace=True)
 
-    # sort index
+    # Concatenate the DataFrames
+    df_total = pd.concat([train_df, test_df, forecast_df, confidence_df], ignore_index=True)
+
+    # Sort index
     df_total = df_total.sort_index()
 
+    # create new figure
+    fig = go.Figure()
+
     # Use Plotly Express to create the line plot
-    fig = px.line(df_total, x=df_total.index, y='value', color='data_type', color_discrete_sequence=['blue', 'red', 'green'])
+    # fig = px.line(df, x=df.index, y='value', color='red')
+
+    # Add the training data to the plot
+    fig.add_trace(go.Scatter(
+        x=train_df.index,
+        y=train_df['value'],
+        mode='lines',
+        showlegend=True,
+        name='Training Data'
+    ))
+
+    # Add the test data to the plot
+    fig.add_trace(go.Scatter(
+        x=test_df.index,
+        y=test_df['value'],
+        mode='lines',
+        showlegend=True,
+        name='Test Data'
+    ))
+
+    # Add the forecast to the plot
+    fig.add_trace(go.Scatter(
+        x=forecast_df.index,
+        y=forecast_df['value'],
+        mode='lines',
+        showlegend=True,
+        name='Forecast'
+    ))
+
+    # Add the confidence intervals to the plot
+    fig.add_trace(go.Scatter(
+        x=confidence_df.index,
+        y=confidence_df['upper y'],
+        mode='lines',
+        line=dict(width=0),
+        showlegend=False,
+        name='upper'
+    ))
+    fig.add_trace(go.Scatter(
+        x=confidence_df.index,
+        y=confidence_df['lower y'],
+        mode='lines',
+        line=dict(width=0),
+        fill='tonexty',
+        fillcolor='rgba(0,0,0,0.2)',
+        showlegend=False,
+        name='lower'
+    ))
 
     # Show the plot
     fig.show()
